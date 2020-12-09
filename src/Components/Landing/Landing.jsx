d80e303a40a603fa9434c5334d1f180834c724b6
jyo-linux <jyokari51@gmail.com>
2020-12-08 22:57:58 -0600

    Agregando contenedores y footer

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,5 +1,10 @@
 import React, { Fragment } from 'react';
 import logo from '../../images/logo.svg';
+import iconFacebook from '../../images/icon-facebook.svg';
+import iconYoutube from '../../images/icon-youtube.svg';
+import iconTwiter from '../../images/icon-twitter.svg';
+import iconPinterest from '../../images/icon-pinterest.svg';
+import iconInstagram from '../../images/icon-instagram.svg';
 import './landing.css';
 
 const Landing = () => {
@@ -19,6 +24,36 @@ const Landing = () => {
                 <div className="request">
                     <button className='btnRequest'>Request Invite</button>
                 </div>
+            </div>
+            
+            <div></div>
+            <div></div>
+            <div></div>
+            <div className='footer'>
+                <div></div>
+                <div>
+                    <img src={ iconFacebook } alt=""/>
+                    <img src={ iconYoutube } alt=""/>
+                    <img src={ iconTwiter } alt=""/>
+                    <img src={ iconPinterest } alt=""/>
+                    <img src={ iconInstagram } alt=""/>
+                </div>
+                <div>
+                    <a href="">About Us</a>
+                    <a href="">Contact</a>
+                    <a href="">Blog</a>
+                </div>
+                <div>
+                    <a href="">Careers</a>
+                    <a href="">Support</a>
+                    <a href="">Provaci Policy</a>
+                </div>
+                <div></div>
+                <div>
+                    <button className='btn-format'>Request Invite</button>
+                    <label htmlFor="">Easebank. All Rights Reserved</label>
+                </div>
+                <div></div>
             </div>
         </Fragment>
      );
